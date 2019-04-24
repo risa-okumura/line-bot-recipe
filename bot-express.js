ba6f27573df5c9e1ed6266fe75ef369a7d6ca90f
risa-okumura <risa.okumura@rakus.co.jp>
2019-04-24 15:05:02 +0900

    アクセストークンを修正

--- bot-express.js
+++ bot-express.js
@@ -6,14 +6,14 @@ const Recipe = require('./src/domain/Recipe.js');
 const fs = require('fs');
 const CATEGORY = require('./src/domain/category.js')
 
-// const config = {
-    //     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    //     channelSecret: process.env.CHANNEL_SECRET,
-    // };
 const config = {
-    channelAccessToken:'GIXXOPV1ocNzO+J2Qov5FpdTFGiT1/Iz1Pr9q+hVlWAa5+4rJkLw1OSGdoVUyJ2GZc+xGvfR3w1JZXVMLR1r4KSISVf2v1B5Sxemrx/bcDpfm63HNNRGuqfYR6M546TBDUdYSi48KUFhcf2SJzQ7JAdB04t89/1O/w1cDnyilFU=',
-    channelSecret:'ce49f5f6fcc3620528178c57d2c6b48e'
-};
+        channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+        channelSecret: process.env.CHANNEL_SECRET,
+    };
+// const config = {
+//     channelAccessToken:'GIXXOPV1ocNzO+J2Qov5FpdTFGiT1/Iz1Pr9q+hVlWAa5+4rJkLw1OSGdoVUyJ2GZc+xGvfR3w1JZXVMLR1r4KSISVf2v1B5Sxemrx/bcDpfm63HNNRGuqfYR6M546TBDUdYSi48KUFhcf2SJzQ7JAdB04t89/1O/w1cDnyilFU=',
+//     channelSecret:'ce49f5f6fcc3620528178c57d2c6b48e'
+// };
 
 const client = new line.Client(config);
     
